827fbb857cf1d46b6720c6f24db5a120bf51c7f2
Kenneth Owusu <k.owusu.me@gmail.com>
2023-05-24 14:46:58 +0000

    remove sidebar brand

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -10,10 +10,14 @@ export function SidebarBrand() {
 	let logoColor = useColorModeValue('navy.700', 'white');
 
 	return (
-		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<HSeparator mb='20px' />
-		</Flex>
+		// <Flex alignItems='center' flexDirection='column'>
+		// 	<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+		// 	<HSeparator mb='20px' />
+		// </Flex>
+
+		<div>
+			<h2>Admin</h2>
+		</div>
 	);
 }
 
